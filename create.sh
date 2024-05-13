c5db963d8d7f7292a8afe228b8a45da821da8bdc
Kevin Napoli <kvnnap@gmail.com>
2024-05-13 12:28:25 +0200

    Minor changes

--- create.sh
+++ create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creating keys and server config"
+
 docker build -t kvnnap/wg-create .devcontainer/
 docker run --rm -it -v $PWD:/home/ubuntu/wg-create -w /home/ubuntu/wg-create --env-file config.env kvnnap/wg-create:latest ./gen.sh
 
